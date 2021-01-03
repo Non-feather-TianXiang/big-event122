064918bfe7b7782613cd27727339e51c3837c00e
pjf <1271220708@qq.com>
2021-01-03 18:41:52 +0800

    day01

--- assets/lib/tinymce/tinymce_setup.js
+++ assets/lib/tinymce/tinymce_setup.js
@@ -6,7 +6,7 @@ function initEditor() {
     directionality: 'ltr',
     //语言选择中文
     language: 'zh_CN',
-    //高度为400
+    // 高度为400
     height: 300,
     statusbar: false,
     width: '100%',
@@ -18,8 +18,7 @@ function initEditor() {
       'codesample imageupload'
     ],
     //工具栏的补丁按钮
-    toolbar:
-      'insertfile undo redo | \
+    toolbar: 'insertfile undo redo | \
        styleselect | \
        bold italic | \
        alignleft aligncenter alignright alignjustify | \
@@ -33,6 +32,6 @@ function initEditor() {
     fontsize_formats: '10pt 12pt 14pt 18pt 24pt 36pt',
     //按tab不换行
     nonbreaking_force_tab: true
-    //   imageupload_url: '/user/submit-image'
+      //   imageupload_url: '/user/submit-image'
   })
 }
---
+++ index/index.js
@@ -0,0 +1,49 @@
+// ----------------------------------获取用户信息
+
+if (localStorage.getItem("token") == null) {
+    location.href = "../login.html";
+}
+
+// 查接口文档
+$.ajax({
+    url: "http://ajax.frontend.itheima.net/my/userinfo",
+    // 语法设置请求头 ：带上token值
+    headers: {
+        Authorization: localStorage.getItem("token")
+    },
+    success: function (res) {
+        var name = res.data.nickname || res.data.username;
+
+        $(".username").html(name);
+
+        // 有头像地址显示图片 ，显示头像
+        // 如果没有，截取名字第一字符，大写，显示在某个盒子内
+        if (res.data.user_pic) {
+            $(".layui-nav-img").show().attr("src", res.data.user_pic);
+            $(".avatar").hide();
+        } else {
+            var first = name.substr(0, 1);
+            first = first.toUpperCase();
+
+
+            $(".layui-nav-img").hide();
+            $(".avatar").show().css("display", "inline-block").text(first);
+        }
+    }
+})
+//--------------------------------退出事件
+var layer = layui.layer;
+$("#logout").on("click", function () {
+    // confirm: 询问
+    //          带两个按钮 ， 确定 ，取消
+    //          参数1：询问的话
+    //          参数2： 点击确认的时候，执行的函数
+    layer.confirm("您确定要退出吗？", function (index) {
+        // index:数值用于关闭当前的弹窗
+
+        // 设计：回到登录   token还在本地存着
+        localStorage.removeItem("token");
+        location.href = "../login.html";
+        layer.close(index);
+    })
+})
